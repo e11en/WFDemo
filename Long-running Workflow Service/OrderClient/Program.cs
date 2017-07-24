60a8702e0ebf80ba144a9f20dec5281c25a30656
elangelaar <ellenlangelaar@gmail.com>
2017-07-24 11:20:52 +0200

    Added the long-running workflow service solution

---
+++ Long-running Workflow Service/OrderClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderClient.OrderService;
+
+namespace OrderClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Send initial message to start the workflow service  
+            Console.WriteLine("Sending start message");
+            var startProxy = new StartOrderClient();
+            string orderId = startProxy.StartOrder("Kim Abercrombie");
+
+            // The workflow service is now waiting for the second message to be sent  
+            Console.WriteLine("Workflow service is idle...");
+            Console.WriteLine("Press [ENTER] to send an add item message to reactivate the workflow service...");
+            Console.ReadLine();
+
+            // Send the second message  
+            Console.WriteLine("Sending add item message");
+            var addProxy = new AddItemClient();
+            var item = new AddItem
+            {
+                p_itemId = "Zune HD",
+                p_orderId = orderId
+            };
+
+            string orderResult = addProxy.AddItem(item);
+            Console.WriteLine("Service returned: " + orderResult);
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # WFDemo
 Code for different tutorials about Workflow Foundation
+
+## Long-running Workflow Service
+The code for the tutorial from [Creating a Long-running Workflow Service](https://docs.microsoft.com/en-us/dotnet/framework/wcf/feature-details/creating-a-long-running-workflow-service).
+
