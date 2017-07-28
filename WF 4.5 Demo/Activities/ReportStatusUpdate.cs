17199162abfed23b6e9ed414d967f1b44450e9d6
elangelaar <ellenlangelaar@gmail.com>
2017-07-28 10:07:31 +0200

    Added activities and empty workflow service

---
+++ WF 4.5 Demo/Activities/ReportStatusUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System.Activities;
+using System.Linq;
+using Contracts;
+
+namespace Activities
+{
+    public sealed class ReportStatusUpdate : CodeActivity
+    {
+        public InArgument<int> ReportID { get; set; }
+        public InArgument<ReportStatusType> Status { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            int id = ReportID.Get(context);
+            ReportStatusType stat = Status.Get(context);
+
+            var ctx = new DAL.SwitchContext();
+            var report = ctx.Switches.Where((r) => r.Id == id).SingleOrDefault();
+            report.Status = (byte)stat;
+
+            ctx.SaveChanges();
+        }
+    }
+}
---
+++ WF 4.5 Demo/Activities/SaveSwitchReport.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+
+namespace Activities
+{
+    public sealed class SaveSwitchReport : CodeActivity<int>
+    {
+
+        public InArgument<Contracts.SwitchReport> Report { get; set; }
+
+        protected override int Execute(CodeActivityContext context)
+        {
+            var report = Report.Get(context);
+            if (report != null)
+            {
+                var rep = new DAL.Switch
+                {
+                    Date = report.Date,
+                    EAN = report.EAN,
+                    Status = (byte)ReportStatusType.Submitted
+                };
+
+                var ctx = new DAL.SwitchContext();
+                ctx.Switches.Add(rep);
+                //ctx.People.AddObject(rep.Employee);
+                ctx.SaveChanges();
+
+                return rep.Id;
+            }
+            return 0;
+        }
+    }
+}
